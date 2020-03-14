98b032243c9fb5f4b989769fc1051c0b8b29c2ef
HarsimranVirk <harsimranvirk99@gmail.com>
2020-03-15 01:04:03 +0530

    Added UI and emboss strength

--- src/main.js
+++ src/main.js
@@ -1,55 +1,68 @@
 import * as PIXI from "pixi.js";
-import EmbossFilter from './Filters/EmbossFilter';
+import EmbossFilter from "./Filters/EmbossFilter";
+
+const canvas = document.querySelector("canvas");
 
 const app = new PIXI.Application({
-  transparent: true,
-	height: 300,
-	width: 400,
+  view: canvas,
+  backgroundColor: 0xEEEEEE,
+  width: 600,
+  height: 500
 });
 
-document.body.appendChild(app.view);
 let texture = null;
 let imgSprite = null;
 const button = document.getElementById("upload_btn");
 const convert = document.getElementById("convert");
+const heightInput = document.getElementById("heightInput");
+const heightSlider = document.getElementById("heightSlider");
+let embossStrength = 5;
 
-button.addEventListener('change', () => {
-	if(button.files && button.files[0]){
-		const file = button.files[0];
-		const reader = new FileReader();
-		reader.readAsDataURL(file);
-		reader.onload = () => {
-			texture = PIXI.Texture.from(reader.result);
-			createSprite(texture);
-		};
-	}
+button.addEventListener("change", () => {
+  if (button.files && button.files[0]) {
+    const file = button.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      texture = PIXI.Texture.from(reader.result);
+      createHeightMap(texture);
+    };
+  }
 });
 
+heightInput.oninput = () => {
+	heightSlider.value = heightInput.value;
+	embossStrength = heightInput.value;
+	createHeightMap(texture);
+}
 
-function createSprite(texture) {
-	if(app.stage.children.length === 1) app.stage.children.pop(); 
+heightSlider.oninput = () => {
+	heightInput.value = heightSlider.value;
+	embossStrength = heightSlider.value;
+	createHeightMap(texture);
+}
+
+function createHeightMap(texture) {
+	console.log(embossStrength);
+	if (app.stage.children.length === 1) app.stage.children.pop();
 	imgSprite = new PIXI.Sprite(texture);
-	imgSprite.filters = [new EmbossFilter(1)];
+	imgSprite.filters = [new EmbossFilter(embossStrength)];
 	app.stage.addChild(imgSprite);
 }
 
 function downloadAsImage(renderer, sprite, fileName) {
-	renderer.extract.canvas(sprite).toBlob(function(b){
-		var a = document.createElement('a');
-		document.body.append(a);
-		a.download = fileName;
-		a.href = URL.createObjectURL(b);
-		a.click();
-		a.remove();
-	}, 'image/png');
+  renderer.extract.canvas(sprite).toBlob((blob) => {
+    var a = document.createElement("a");
+    document.body.append(a);
+    a.download = fileName;
+    a.href = URL.createObjectURL(blob);
+    a.click();
+    a.remove();
+  }, "image/png");
 }
 
-convert.addEventListener('click', () => {
-	if(imgSprite) {
-		downloadAsImage(app.renderer, imgSprite, `heightmap${Date.now()}`);
-	}
-})
-
-
-
-
+convert.addEventListener("click", () => {
+  if (imgSprite) {
+    downloadAsImage(app.renderer, imgSprite, `heightmap${Date.now()}`);
+  }
+});
