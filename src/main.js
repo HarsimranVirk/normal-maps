ab257b65713c5f9b6ac67e6c40377cdd88e4fd7e
HarsimranVirk <harsimranvirk99@gmail.com>
2020-03-15 16:11:19 +0530

    Added pixi layers and pixi lights

--- src/main.js
+++ src/main.js
@@ -8,11 +8,12 @@ const app = new PIXI.Application({
   view: canvas,
   backgroundColor: 0xEEEEEE,
   width: 600,
-  height: 500
+  height: 600
 });
 
 let texture = null;
 let imgSprite = null;
+let size = null;
 const button = document.getElementById("upload_btn");
 const convert = document.getElementById("convert");
 const heightInput = document.getElementById("heightInput");
@@ -20,51 +21,66 @@ const heightSlider = document.getElementById("heightSlider");
 let embossStrength = 5;
 let normalStrength = 1;
 
-button.addEventListener("change", () => {
+button.onchange = () => {
   if (button.files && button.files[0]) {
     const file = button.files[0];
     const reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = () => {
       texture = PIXI.Texture.from(reader.result);
-      createHeightMap(texture);
+      createNormalMap(texture, false);
     };
   }
-});
+};
 
 heightInput.oninput = () => {
 	heightSlider.value = heightInput.value;
 	embossStrength = heightInput.value;
-	createHeightMap(texture);
+	createNormalMap(texture, false);
 }
 
 heightSlider.oninput = () => {
 	heightInput.value = heightSlider.value;
 	embossStrength = heightSlider.value;
-	createHeightMap(texture);
+	createNormalMap(texture, false);
 }
 
-function createHeightMap(texture) {
-	console.log(embossStrength);
-	if (app.stage.children.length === 1) app.stage.children.pop();
+canvas.onwheel = (event) => {
+  if(imgSprite) {
+    size = event.deltaY * -0.001;
+    imgSprite.scale.set(Math.max(imgSprite.scale.x - size, 0.125));
+    size = imgSprite.scale.x;
+  }
+}
+
+function createNormalMap(texture, downloadMode) {
+	if (app.stage.children.length === 1) app.stage.removeChild(app.stage.children[0]);
+  if(imgSprite) imgSprite = null;
 	imgSprite = new PIXI.Sprite(texture);
 	imgSprite.filters = [new EmbossFilter(embossStrength), new NormalFilter(normalStrength)];
-	app.stage.addChild(imgSprite);
+  if(!downloadMode) {
+    imgSprite.position.set(300,300);
+    imgSprite.anchor.set(0.5);
+    if(size !== null) imgSprite.scale.set(size);
+  }
+  app.stage.addChild(imgSprite);
 }
 
 function downloadAsImage(renderer, sprite, fileName) {
-  renderer.extract.canvas(sprite).toBlob((blob) => {
+    const image = renderer.plugins.extract.image(sprite);
     var a = document.createElement("a");
     document.body.append(a);
     a.download = fileName;
-    a.href = URL.createObjectURL(blob);
+    a.href = image.src;
     a.click();
     a.remove();
-  }, "image/png");
+    createNormalMap(texture, false);
 }
 
 convert.addEventListener("click", () => {
   if (imgSprite) {
-    downloadAsImage(app.renderer, imgSprite, `heightmap${Date.now()}`);
+    createNormalMap(texture, true);
+    downloadAsImage(app.renderer, imgSprite, `normalmap${Date.now()}`);
   }
 });
+
