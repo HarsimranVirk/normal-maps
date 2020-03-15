b76e0a05e5274667c2155d7ed379c482e2fa6418
HarsimranVirk <harsimranvirk99@gmail.com>
2020-03-16 00:16:16 +0530

    added preview funtionality

--- src/main.js
+++ src/main.js
@@ -1,22 +1,18 @@
-import * as PIXI from "pixi.js";
+import "pixi.js";
 import 'pixi-layers';
-import 'pixi-lights';
+import * as lights from 'pixi-lights';
 import EmbossFilter from "./Filters/EmbossFilter";
 import NormalFilter from "./Filters/NormalFilter";
-import { createAppStage, createPair } from './preview';
 
-const canvas = document.querySelector("canvas");
+const canvasDiv = document.getElementById("canvas");
 
-const app = new PIXI.Application({
-  view: canvas,
-  backgroundColor: 0xEEEEEE,
-  width: 600,
-  height: 600
-});
+
 
 let texture = null;
 let imgSprite = null;
 let size = null;
+let app = null;
+let previewApp = null;
 const button = document.getElementById("upload_btn");
 const convert = document.getElementById("convert");
 const heightInput = document.getElementById("heightInput");
@@ -24,6 +20,28 @@ const heightSlider = document.getElementById("heightSlider");
 const previewBtn = document.getElementById("preview");
 let embossStrength = 5;
 let normalStrength = 1;
+
+window.onload = () => {
+  renderNormal();
+}
+
+const renderNormal = () => {
+    convert.disabled = false;
+    if (previewApp !== null) {
+      canvasDiv.removeChild(previewApp.view);
+      previewApp.destroy();
+      previewApp = null;
+    } 
+    app = new PIXI.Application({
+    backgroundColor: 0xEEEEEE,
+    width: 600,
+    height: 500
+  });
+  canvasDiv.appendChild(app.view);
+  app.view.onwheel = (event) => moveWheel(event);
+  if (texture !== null) createNormalMap(texture, false);
+}
+
 
 button.onchange = () => {
   if (button.files && button.files[0]) {
@@ -49,7 +67,7 @@ heightSlider.oninput = () => {
 	createNormalMap(texture, false);
 }
 
-canvas.onwheel = (event) => {
+const moveWheel = (event) => {
   if(imgSprite) {
     size = event.deltaY * -0.001;
     imgSprite.scale.set(Math.max(imgSprite.scale.x - size, 0.125));
@@ -63,11 +81,12 @@ function createNormalMap(texture, downloadMode) {
 	imgSprite = new PIXI.Sprite(texture);
 	imgSprite.filters = [new EmbossFilter(embossStrength), new NormalFilter(normalStrength)];
   if(!downloadMode) {
-    imgSprite.position.set(300,300);
+    imgSprite.position.set(300,250);
     imgSprite.anchor.set(0.5);
     if(size !== null) imgSprite.scale.set(size);
   }
   app.stage.addChild(imgSprite);
+  previewBtn.disabled = false;
 }
 
 function downloadAsImage(renderer, sprite, fileName) {
@@ -89,17 +108,53 @@ convert.addEventListener("click", () => {
 });
 
 previewBtn.onclick = () => {
-  if(texture && imgSprite){
-    app.destroy();
-    const previewApp = createAppStage();
-    const light = new PIXI.lights.PointLight(0xffffff,1);
-    const pair = createPair(texture, imgSprite);
-    pair.position.set(300,300);
-    light.position.set(525, 160);
-    previewApp.stage.addChild(pair);
-    previewApp.stage.addChild(light);
-    previewApp.stage.addChild(new PIXI.lights.AmbientLight(null, 0.4));
-
-  }
+  if (!convert.disabled){
+    renderPreview();
+    previewBtn.innerHTML = 'Back';
+  } 
+  else {
+    renderNormal();
+    previewBtn.innerHTML = 'Preview';
+  } 
 }
 
+const renderPreview = () => {
+  convert.disabled = true; 
+  canvasDiv.removeChild(app.view);
+  if(app !== null) app.destroy();
+  app = null;
+  previewApp = new PIXI.Application({
+    height: 500,
+    width: 600,
+    backgroundColor: 0x000000,
+    });
+  canvasDiv.appendChild(previewApp.view);
+  const stage = previewApp.stage = new PIXI.display.Stage();
+  const light = new lights.PointLight(0xffffff, 1);
+  stage.addChild(new PIXI.display.Layer(lights.diffuseGroup));
+  stage.addChild(new PIXI.display.Layer(lights.normalGroup));
+  stage.addChild(new PIXI.display.Layer(lights.lightGroup));
+
+  const pair = new PIXI.Container();
+  const diffuseSprite = new PIXI.Sprite(texture);
+  diffuseSprite.parentGroup = lights.diffuseGroup;
+  imgSprite.parentGroup = lights.normalGroup;
+  pair.addChild(diffuseSprite);
+  pair.addChild(imgSprite);
+  light.position.set(300,300);
+  pair.position.set(25);
+  stage.addChild(pair);
+  stage.addChild(new lights.AmbientLight(null, 0.8));
+  stage.addChild(new lights.DirectionalLight(null, 1, pair));
+  stage.addChild(light);
+  stage.interactive = true;
+  stage.on('mousemove', function(event) {
+    light.position.copy(event.data.global);
+    });
+
+  stage.on('pointerdown', function(event) {
+    var clickLight = new lights.PointLight(0xffffff);
+    clickLight.position.copy(event.data.global);
+    stage.addChild(clickLight);
+  });
+}
